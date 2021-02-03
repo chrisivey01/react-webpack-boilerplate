3ddeaf5f32d47c3d8d04e58182eed38e78268f63
Chris <chrisivey01@gmail.com>
2021-02-03 10:18:12 -0600

    fixed paths.js

--- webpack/paths.js
+++ webpack/paths.js
@@ -5,7 +5,7 @@ module.exports = {
   src: path.resolve(__dirname, '../src'),
 
   // Production build files
-  build: path.resolve("C:/FXServer/server-data/resources/pma-gang-framework/gta/dist"),
+  build: path.resolve("../.."),
 
   // Static files that get copied to build folder
   public: path.resolve(__dirname, '../public'),
